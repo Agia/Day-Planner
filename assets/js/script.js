8f6fde049ce127518346cea6d601727d86b0b6c9
Agia <agia@pm.me>
2023-01-19 22:11:03 +0000

    Added agenda hourly inputs to HTML, along with CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
 // Get the current day / date and render it to the target element
 let currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDate);
-
-// let $currentDate = $("#currentDay")
-// setInterval( function () {
-//     currentDay.text(moment().format("dddd, MMM Do YYYY"));
-// }, 60000)
