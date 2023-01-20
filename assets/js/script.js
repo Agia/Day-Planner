b3365192e510f9e501e5ae70ca932316bd03b2af
Agia <agia@pm.me>
2023-01-20 18:20:46 +0000

    Removed unused fonts & classes, added a event listener on the save buttons that stores input and realted time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,13 @@
 let currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDate);
 
+
+// * EVENT LISTENERS * //
+$(".button-save").on("click", function () {
+    let input = $(this).siblings(".event-input").val();
+    let time = $(this).siblings(".event-input").attr("data-time");
+    // console.log(input + " " + time);
+    localStorage.setItem(time, input); // Check best order for variables
+})
+
+
